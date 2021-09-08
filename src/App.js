bec934c4d0d555ff045276b56870a3ed5e634135
DmVal2020 <blohala90@gmail.com>
2021-09-08 15:29:53 +0300

    добавил блок авторизации, но пока нет планов его использовать

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from './hoc/Layout/Layout'
 import Libs from './containers/Libs/Libs'
-// import Auth from './containers/Auth/Auth'
+import Auth from './containers/Auth/Auth'
 // import Categories from './containers/Categories/Categories'
 // import Autors from './containers/Autors/Autors'
 // import Books from './containers/Books/Books'
@@ -13,7 +13,8 @@ function App(){
       <Layout>
         <Switch>
           <Route path="/" exact component={Libs}/>
-          {/* <Route path="/auth"  component={Auth}/>
+          <Route path="/auth"  component={Auth}/>
+          {/* 
           <Route path="/categories"  component={Categories}/>
           <Route path="/autors"  component={Autors}/>
           <Route path="/books"  component={Books}/>
--- src/containers/Libs/Libs.js
+++ src/containers/Libs/Libs.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import classes from './Libs.css'
+import {Link} from 'react-router-dom'
 
 class Libs extends Component{
+    clickButtonHandler(){
+
+    }
     render(){
         return(
             <div className={classes.Libs}>
                 <div>
                     <h1>Добро пожаловать в домашнюю библиотеку</h1>
-                <button>Войти в библиотеку</button>
+                    <Link to="/categories">
+                    <button>Войти в библиотеку</button>
+                    </Link>
                 </div>               
             </div>
         )
